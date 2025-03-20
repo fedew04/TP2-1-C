51aaddf15e102a617ae6582c63e6b38c4331e65b
MatiasDuro-ORT <matias.duro@ort.edu.ar>
2025-03-19 22:07:32 -0300

    Clase 1 finalizada

---
+++ Clase-1/Arrays.js
@@ -0,0 +1,110 @@
+let frutas = ["Banana", "Manzana", "Frutilla"]
+
+// console.log(frutas);
+
+// METODOS DE ARRAYS
+
+// push agrega un elemento al final del array
+frutas.push("Uva")
+
+// console.log(frutas);
+
+// pop elimina el ultimo elemento del array
+frutas.pop()
+
+// console.log(frutas);
+
+// unshift agrega un elemento al comienzo del array
+frutas.unshift("Mango")
+
+// console.log(frutas);
+
+// shift elimina el primer elemento del array
+frutas.shift()
+
+// console.log(frutas);
+
+
+let numeros = [1, 2, 3, 4, 5]
+
+// console.log("Numeros: ", numeros);
+
+
+
+// Transforma cada elemento del array, y devuelve uno nuevo, segun la funcion
+let nuevoArray = numeros.map(num => num * num)
+
+console.log(nuevoArray);
+
+// Filtra los elementos 1 por 1 segun la condicion y devuelve un nuevo array
+let mayores = numeros.filter(numero => numero >= 3)
+
+console.log(mayores);
+
+
+// reduce, reduce el array a un solo valor, acumulandolo en el acumulador
+let sumaArray = numeros.reduce((acumulador, num) => acumulador + num, 0) 
+
+console.log(sumaArray);
+
+// El find devuelve el PRIMER elemento que cumpla con la condicion
+let busqueda = numeros.find( num => num >= 2);
+console.log(busqueda);
+
+// Some devuelve true si al menos un elemento cumple con la condicion
+let encontrado = numeros.some( num => num == 6)
+
+console.log("Algun elemento cumple la condicion? ", encontrado);
+
+// Every devuelve true si TODOS los elementos cumplen la condicion
+let encontrado2 = numeros.every( num => num < 5)
+
+console.log("Algun elemento cumple la condicion? ", encontrado2);
+
+
+// Convierte el array a string y los separa a cada elemento por el separador que indiquemos en el join
+let nuevoString = numeros.join(" /// ")
+
+console.log(nuevoString);
+
+// Invierte el array
+let invertido = numeros
+
+invertido.reverse()
+
+numeros.reverse()
+
+// extrae una parte del array sin modificar el original
+// let arrayNuevo = numeros.slice(2, 5)
+
+// console.log(arrayNuevo);
+
+console.log(numeros);
+
+// modifica el array eliminando elemento y reemplazandolo por otro, retorna el elemento eliminado
+let reemplazo = numeros.splice(2, 1, 99)
+
+console.log(numeros);
+console.log(reemplazo);
+
+//Concatena el array con otro
+let arrayConcatenado = numeros.concat(frutas)
+console.log(arrayConcatenado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Clase-1/Variables.js
@@ -0,0 +1,42 @@
+// Hay 3 tipos de variables
+
+// var
+// let
+// const
+
+// Var (NO ES RECOMENDADO): Es reasignable, y redeclarable.
+var nombre = "Matias"
+
+var nombre = "Juan"
+
+console.log("Nombre: ", nombre);
+
+
+// let: Puede Reasignarse, pero no redeclararse
+let edad = 28
+edad = 30 // Se reasigna
+// let edad = 32 
+
+// Const No se puede reasignar ni redeclarar
+const apellido = "Duro"
+// apellido = "Perez" // No se puede re asignar 
+
+
+var variable1
+
+console.log("Variable 1: ", variable1)
+
+let variable2
+console.log("Variable 2: ", variable2)
+
+variable2 = 28
+console.log("Variable 2: ", variable2)
+
+
+
+// Const debe ser asignada al momento de declararse
+// const variable3
+
+// console.log("Variable 3: ", variable3);
+
+
