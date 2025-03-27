7b5e91c8cb00644ab39776b6134ea0e6a782a10b
fedew04 <fedewainstein@gmail.com>
2025-03-26 22:48:58 -0300

    ej 1 y 2

--- Clase-2/Ejercicios/Ejercicio1.js
+++ Clase-2/Ejercicios/Ejercicio1.js
@@ -2,28 +2,55 @@ console.log("Arranca el ejercicio");
 
 // Identificar los palindromos
 
-function invertirTexto(){
-    // Aca invierte el texto
+function invertirTexto(palabra) {
+  // Aca invierte el texto
+  let palabraInvertida = "";
+  for (i = palabra.length; i >= 0; i--) {
+    palabraInvertida += palabra.charAt(i);
+  }
+
+  console.log(palabraInvertida);
+  return palabraInvertida;
 }
 
-function esPalindromo(palabra){
-    const palabraMinuscula = palabra.tolowerCase() // Cambia toda la palabra a minuscula
-    const palabraInvertida = invertirTexto(palabraMinuscula)
-    // Aca verifica si es palindromo
+function esPalindromo(palabra) {
+  const palabraMinuscula = palabra.tolowerCase(); // Cambia toda la palabra a minuscula
+  const palabraInvertida = invertirTexto(palabraMinuscula);
+  for (let i = 0; i < palabraMinuscula.length; i++) {
+    if (palabraMinuscula[i] !== palabraInvertida[i]) {
+      return false;
+    }
+  }
+  // Aca verifica si es palindromo
 }
 
-function verificarPalindromo(){
-// Separen en 2 arrays, las palabras palindromos y las que no lo son
+function verificarPalindromo() {
+  // Separen en 2 arrays, las palabras palindromos y las que no lo son
 
+  esPalindromo(palabra);
 
-    esPalindromo(palabra)
-
-    return // esos dos arrays
+  return; // esos dos arrays
 }
 
-const palabras = ["Neuquen", "Sol", "Hola", "Ana", "Oso", "Saludo", "Reconocer"]
+const palabras = [
+  "Neuquen",
+  "Sol",
+  "Hola",
+  "Ana",
+  "Oso",
+  "Saludo",
+  "Reconocer",
+];
 
-verificarPalindromo(palabras)
+let palabra = "hola";
+let palabraInvertida = "";
+for (i = palabra.length; i >= 0; i--) {
+  palabraInvertida += palabra.charAt(i);
+}
+
+console.log(palabraInvertida);
+
+//verificarPalindromo(palabras);
 
 // console.log(verificarPalindromo(palabras));
 
