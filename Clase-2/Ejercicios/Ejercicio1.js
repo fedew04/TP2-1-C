dd354d4991f1b46d02cd758e67c166fbc24f0bba
Matias Duro <matias.duro@ina.edu.ar>
2025-03-26 21:38:57 -0300

    Ejercicio 1 para  clase 2

---
+++ Clase-2/Ejercicios/Ejercicio1.js
@@ -0,0 +1,30 @@
+console.log("Arranca el ejercicio");
+
+// Identificar los palindromos
+
+function invertirTexto(){
+    // Aca invierte el texto
+}
+
+function esPalindromo(palabra){
+    const palabraMinuscula = palabra.tolowerCase() // Cambia toda la palabra a minuscula
+    const palabraInvertida = invertirTexto(palabraMinuscula)
+    // Aca verifica si es palindromo
+}
+
+function verificarPalindromo(){
+// Separen en 2 arrays, las palabras palindromos y las que no lo son
+
+
+    esPalindromo(palabra)
+
+    return // esos dos arrays
+}
+
+const palabras = ["Neuquen", "Sol", "Hola", "Ana", "Oso", "Saludo", "Reconocer"]
+
+verificarPalindromo(palabras)
+
+// console.log(verificarPalindromo(palabras));
+
+// deberia devolver  2 arrays, uno con las palabras palindromas y otro con las que normales
