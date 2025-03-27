234bdcb8bb4c3c180eeaff96c9ae56c7f16e299d
Matias Duro <matias.duro@ina.edu.ar>
2025-03-26 22:06:23 -0300

    segundo ejercicio clase 2

---
+++ Clase-2/Ejercicios/Ejercicio2.js
@@ -0,0 +1,34 @@
+
+function esMayor(){
+
+}
+
+function esHombre(){
+
+}
+
+
+function SepararUsuarios(){
+
+
+    let objetoFinal = {
+        mayores:{
+            // hombres:
+            // mujeres:
+        },
+        menores:{
+            // hombres:
+            // mujeres:
+        }
+    }
+}
+
+const usuarios = [
+    {nombre: "Lucas", edad: 27, sexo: "M"},
+    {nombre: "Daniela", edad: 25, sexo: "F"},
+    {nombre: "Martin", edad: 17, sexo: "M"},
+    {nombre: "Delfina", edad: 22, sexo: "F"},
+    {nombre: "Fernando", edad: 15, sexo: "M"},
+]
+
+console.log(SepararUsuarios(usuarios));
